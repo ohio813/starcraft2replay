c0cf465ff861dcab80d0bec600b88d2972c4e9ea
vallee.aurelien <vallee.aurelien@1dbccace-f34c-6c77-4bfe-38e296418b7d>
2010-03-25 08:40:35 +0000

    Initial import from previous SVN

---
+++ trunk/tools/extract_messageevents_dump.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstdlib>
+
+#include "sc2replay/replay.h"
+#include "sc2replay/messageevents.h"
+
+namespace sc = sc2replay;
+
+int main( int argc, char** argv )
+{
+  if ( argc != 3 )
+  {
+    std::cerr << "Replay file does not exists or is invalid." << std::endl;
+    std::cerr << "If you are using libmpq (default) this may randomly happen. ";
+    std::cerr << "Try to install StormLib (http://www.zezula.net/en/mpq/stormlib.html) ";
+    std::cerr << "and use the CMake option -DBUILD_LIBMPQ=OFF" << std::endl;
+    return EXIT_FAILURE;
+  }
+  
+  sc::Replay replay( argv[1] );
+  
+  if ( !replay.isValid() )
+  {
+    std::cerr << "Replay file does not exists or is invalid." << std::endl;
+    return EXIT_FAILURE;
+  }
+  
+  replay.getMessageEvents().exportDump( argv[2] );
+  
+  std::cout << "Replay message events successfully extracted to \"" << argv[2] << "\"." << std::endl;
+  
+  return EXIT_SUCCESS;
+}
