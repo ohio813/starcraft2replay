41b0dc40636dffdf3bc98e51ccb576986cf00486
vallee.aurelien <vallee.aurelien@1dbccace-f34c-6c77-4bfe-38e296418b7d>
2010-03-25 08:50:28 +0000

    Added some dumpers

--- trunk/tools/extract_info_dump.cpp
+++ trunk/tools/extract_info_dump.cpp
@@ -10,10 +10,7 @@ int main( int argc, char** argv )
 {
   if ( argc != 3 )
   {
-    std::cerr << "Replay file does not exists or is invalid." << std::endl;
-    std::cerr << "If you are using libmpq (default) this may randomly happen. ";
-    std::cerr << "Try to install StormLib (http://www.zezula.net/en/mpq/stormlib.html) ";
-    std::cerr << "and use the CMake option -DBUILD_LIBMPQ=OFF" << std::endl;
+    std::cerr << "Usage: extract_info_dump <replay file> <dump file>" << std::endl;
     return EXIT_FAILURE;
   }
   
@@ -22,6 +19,9 @@ int main( int argc, char** argv )
   if ( !replay.isValid() )
   {
     std::cerr << "Replay file does not exists or is invalid." << std::endl;
+    std::cerr << "If you are using libmpq (default) this may randomly happen. ";
+    std::cerr << "Try to install StormLib (http://www.zezula.net/en/mpq/stormlib.html) ";
+    std::cerr << "and use the CMake option -DBUILD_LIBMPQ=OFF" << std::endl;
     return EXIT_FAILURE;
   }
   
