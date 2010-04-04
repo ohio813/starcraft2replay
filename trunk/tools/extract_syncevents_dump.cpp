c431d6d626e0c518c9d2871c459aec56d3c8633e
vallee.aurelien <vallee.aurelien@1dbccace-f34c-6c77-4bfe-38e296418b7d>
2010-04-04 18:49:15 +0000

    Added message dumps

--- trunk/tools/extract_syncevents_dump.cpp
+++ trunk/tools/extract_syncevents_dump.cpp
@@ -25,7 +25,7 @@ int main( int argc, char** argv )
     return EXIT_FAILURE;
   }
   
-  replay.getMessageEvents().exportDump( argv[2] );
+  replay.getSyncEvents().exportDump( argv[2] );
   
   std::cout << "Replay sync events successfully extracted to \"" << argv[2] << "\"." << std::endl;
   
