68b22a3d7bb8832b171e8c873f1ed47439594d76
zeta.two <zeta.two@1dbccace-f34c-6c77-4bfe-38e296418b7d>
2010-04-21 21:24:51 +0000

    Removed duplicate folder (name change)

--- trunk/php/extension-new/zend_include.h
+++
@@ -1,9 +0,0 @@
-#ifndef ZEND_INCLUDE_H
-#define ZEND_INCLUDE_H
-
-// Include the Zend Win32 header and the general PHP header 
-#include "zend_config.w32.h" 
-#include "php.h"
-#include "ext/standard/info.h"
-
-#endif
